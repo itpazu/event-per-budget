801f29feae577fe0866bc37c7362412508c5e2b9
itpazu <itpazu@gmail.com>
2023-08-14 22:20:42 +0300

    fixing env var

--- src/app/getData.ts
+++ src/app/getData.ts
@@ -58,8 +58,7 @@ export type FetchEvent = StrapiData<Omit<EventProps, "category" | "id"> &
 export async function getFromStrapi<AT>(path: string, urlParams: {} | null = null,
     chacheObj: { [key: string]: { [key: string]: number } } | { [key: string]: string } = { next: { revalidate: 100 } }): Promise<AT> {
 
-    if (process.env.NODE_ENV === "production" || process.env.CURRENT_ENV === "production") {
-
+    if (process.env.VERCEL_ENV === "production") {
         const BASE_URL = process.env.STRAPI_URL_PRODUCTION
         const searchParams = urlParams ? qs.stringify(urlParams, {
             encodeValuesOnly: true
